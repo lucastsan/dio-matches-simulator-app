bf3da81d4a714279730bed64295c158852b960e6
Lucas Santos <lucastsantos1020@gmail.com>
2022-06-24 22:59:37 -0300

    return to main screen in manifest

--- app/src/main/java/me/lucastsan/simulator/MainActivity.kt
+++ app/src/main/java/me/lucastsan/simulator/MainActivity.kt
@@ -1,11 +1,17 @@
 package me.lucastsan.simulator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.lucastsan.simulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
